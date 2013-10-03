b0a63aba6eaff9ae554679b6c1e29bbc87e3b25c
Erick Caraballo <erick.caraballo@upr.edu>
2013-10-03 13:08:10 -0400

    First commit.

---
+++ src/net/skillfullearning/contactslist/MainActivity.java
@@ -0,0 +1,29 @@
+package net.skillfullearning.contactslist;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+/**
+ * Display a welcome screen with
+ * logo and name of the app.
+ * 
+ * @author cyb
+ *
+ */
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
---
+++ src/net/skillfullearning/lists/SortedArrayList.java
@@ -0,0 +1,99 @@
+package net.skillfullearning.lists;
+
+import java.util.Iterator;
+
+public class SortedArrayList implements SortedList<String> {
+	
+	public SortedArrayList() { }
+
+	@Override
+	public Iterator<String> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean add(String obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean remove(String obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(int index) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int removeAll(String obj) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String first() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String last() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean contains(String e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterator<String> iterator(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int firstIndex(String e) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int lastIndex(String e) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
